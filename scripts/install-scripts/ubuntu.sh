44b9a89285b1da60df5ca4c22f11b1558186740e
Akira Kido <achts.y@gmail.com>
2018-01-11 19:29:27 +0900

    fixed ubuntu script

--- scripts/install-scripts/ubuntu.sh
+++ scripts/install-scripts/ubuntu.sh
@@ -2,13 +2,30 @@
 
 INSTALL_PATH="/usr/bin/multipftest"
 
-echo "Copying files..."
+if [ -d "${INSTALL_PATH}" ]; then
 
-rm -rf ${INSTALL_PATH}
-cp -R ./publish/. ${INSTALL_PATH}
+    read -p "Program already exists. Would you like to overwrite? [y]" CONFIRM
+    if [ "${CONFIRM}" != "y" ]; then
+        echo "Aborting install."
+        exit
+    fi
 
+    echo "Removing existing directory..."
+    sudo rm -rf ${INSTALL_PATH}
+fi
+
+echo "Copying files to ${INSTALL_PATH}..."
+
+sudo cp -R ./publish/. ${INSTALL_PATH}
+sudo chmod -R 777 ${INSTALL_PATH} 
+
+grep "${INSTALL_PATH}" ~/.profile
+
+if [ $? -eq 1 ]; then
 echo "Adding to path..."
-
 echo "PATH=\"${INSTALL_PATH}:\${PATH}\"" >> ~/.profile
+fi
 
 echo "DONE! ☕"
+
+echo "Execute 'source ~/.profile' to apply the changes immediately."
